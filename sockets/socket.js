1724c2da01f974a0fed814bb420d75f4ba479e51
Alexander Torres <alexskate19@gmail.com>
2022-08-05 19:09:52 -0500

    Server Terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,59 @@
+const {io} = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band')
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del silencio'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Da paw'));
+bands.addBand(new Band('Maquina camaleon'));
 
-const {io}=require('../index');
+console.table(bands.getBands())
 
 
 io.on("connection", (client) => {
     console.log("Client connected");
-  
+
+
+    client.emit('active-bands', bands.getBands());
+
     client.on("disconnect", () => {
-      console.log("cliente disconnected");
+        console.log("cliente disconnected");
     });
-  
-    client.on('mensaje',(payload)=>{
-      console.log(payload)
-      io.emit('mensajes',{admin:'Nuevo mensaje'})
+
+    client.on('mensaje', (payload) => {
+        console.log(payload)
+        io.emit('mensajes', {admin: 'Nuevo mensaje'})
     });
-  
-  
-  });
+
+    client.on('emitir-mensaje', (payload) => {
+        console.log(payload)
+        //emite a todos..!!
+        //io.emit('nuevo-mensaje',payload)
+        client.broadcast.emit('nuevo-mensaje', payload)
+    });
+
+    client.on('votos', (payload) => {
+        console.log(payload)
+        bands.voteBand(payload.id)
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    client.on('save', (payload) => {
+        console.log(payload)
+        const newBand= new Band(payload.name);
+        bands.addBand(newBand)
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete',(payload)=>{
+        console.log(payload)
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    })
+
+
+
+});
